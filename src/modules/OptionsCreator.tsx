a50d4a2e7e88b42545773e8fd0c3839d08f5c3e4
Renwell Queyquep <renwell@excel.local>
2024-09-19 13:24:45 -0700

    Changed setListOfLots to sortListOfLots

--- src/modules/OptionsCreator.tsx
+++ src/modules/OptionsCreator.tsx
@@ -529,7 +529,7 @@ function OptionsCreator() {
             //Accessing existing Job Document
             if(Object.prototype.hasOwnProperty.call(loadedData, 'jobDetails')) {
                 setJobDetails(loadedData.jobDetails)
-                setListOfLots(loadedData.listOfLots)
+                sortListOfLots(loadedData.listOfLots)
                 setCurrentLot(loadedData.listOfLots[0])
                 setCurrentLotNum(loadedData.listOfLots[0].lot)
             } else {
@@ -548,7 +548,7 @@ function OptionsCreator() {
                     date: "",
                     prodReady: false
                 })
-                setListOfLots(loadedData.listOfLots)
+                sortListOfLots(loadedData.listOfLots)
                 setCurrentLot(loadedData.listOfLots[0])
                 setCurrentLotNum(loadedData.listOfLots[0].plan)
                 setPackageProjects(loadedData.packageDetails.projects)
